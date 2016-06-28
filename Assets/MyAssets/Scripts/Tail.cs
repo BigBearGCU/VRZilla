c10e2d0e25bfcdbb53591bb3cf78796b9c23f699
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-28 11:10:01 +0100

    Start of tail
    Now has a tail attached to the left controller - make sure to wear it
    behind you

--- Assets/MyAssets/Scripts/Tail.cs
+++ Assets/MyAssets/Scripts/Tail.cs
@@ -2,22 +2,17 @@
 using System.Collections;
 
 public class Tail : MonoBehaviour {
+	public float tailDistanceXZFromHead = 0.1f;
+	public float tailDistanceYFromHead = 0.5f;
+
+	public GameObject tailController = null;
+	public GameObject headController = null;
 
 	private void Awake()
 	{
 
 
 	}
-
-	private void CreateTailSegment()
-	{
-		GameObject segment = new GameObject ();
-		segment.transform.name = "Tail" + 0;
-
-		//BMD - for Hamid, commenting this out so I can add to version control
-		//segment.AddComponent(new UnityEngine
-	}
-
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +21,21 @@ public class Tail : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (tailController == null || headController == null) {
+			return;
+		}
+
+		Vector3 direction = tailController.transform.position - headController.transform.position;
+		direction.y = 0.0f;
+		direction.Normalize ();
+
+		if(direction.sqrMagnitude <= 0.0f)
+		{
+			return;
+		}
+
+		transform.rotation = Quaternion.LookRotation (direction);
+		transform.Rotate (new Vector3(90.0f, 0.0f, 0.0f), Space.Self);
+		transform.position = headController.transform.position + (direction * tailDistanceXZFromHead) - (Vector3.up * tailDistanceYFromHead);
 	}
 }
