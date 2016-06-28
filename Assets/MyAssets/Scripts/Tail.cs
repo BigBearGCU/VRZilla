ecdb23b08e5467b02c470f8fa6582ed5d6abbf8b
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-28 12:33:32 +0100

    Fixed some instability issues with tail

--- Assets/MyAssets/Scripts/Tail.cs
+++ Assets/MyAssets/Scripts/Tail.cs
@@ -20,7 +20,7 @@ public class Tail : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if (tailController == null || headController == null) {
 			return;
 		}
