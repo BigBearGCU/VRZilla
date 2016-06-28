75da37441b5992f6d98914ef6ee4ca5c5e9e0dd7
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-28 09:48:30 +0100

    First check in

---
+++ Assets/Tail.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tail : MonoBehaviour {
+
+	private void Awake()
+	{
+
+
+	}
+
+	private void CreateTailSegment()
+	{
+		GameObject segment = new GameObject ();
+		segment.transform.name = "Tail" + 0;
+
+		//BMD - for Hamid, commenting this out so I can add to version control
+		//segment.AddComponent(new UnityEngine
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
