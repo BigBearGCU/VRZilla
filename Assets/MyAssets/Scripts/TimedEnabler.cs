34c68045cc7df837c22df7a415152a7c2aa06152
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-28 13:50:28 +0100

    Stripped scene of joints

---
+++ Assets/MyAssets/Scripts/TimedEnabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedEnabler : MonoBehaviour {
+
+	HingeJoint joint;
+	// Use this for initialization
+	void Start () {
+		joint = GetComponent<HingeJoint> ();
+		joint.autoConfigureConnectedAnchor = false;
+		StartCoroutine (enabledTimed());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator enabledTimed()
+	{
+		yield return new WaitForSeconds (0.25f);
+		joint.autoConfigureConnectedAnchor = true;
+
+	}
+}
