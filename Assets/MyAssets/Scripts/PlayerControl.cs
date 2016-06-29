80a69a31261247c539273d35ea3a0f9582bd408f
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-29 18:34:48 +0100

    Added Animation to test scene

---
+++ Assets/MyAssets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed=1.0f;
+	public float turnSpeed=1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vertAxis = Input.GetAxis ("Vertical");
+		float horiAxis = Input.GetAxis ("Horizontal");
+
+		transform.Translate(transform.forward * (Time.deltaTime*speed*vertAxis));
+		transform.RotateAround (transform.position, Vector3.up, turnSpeed * Time.deltaTime * horiAxis);
+	}
+}
---
+++ Assets/MyAssets/Scripts/ZillaAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZillaAnimationController : MonoBehaviour {
+
+	Animator animator;
+	public float animationUpdateRate = 0.5f;
+	public float speed;
+	public Vector3 lastPosition;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		InvokeRepeating("UpdateAnimation", animationUpdateRate,animationUpdateRate);
+		lastPosition = transform.position;
+	}
+
+
+	void UpdateAnimation()
+	{
+		Vector3 diff = transform.position - lastPosition;
+		speed = diff.magnitude;
+		lastPosition = transform.position;
+		animator.SetFloat ("speed", speed);
+	}
+}
