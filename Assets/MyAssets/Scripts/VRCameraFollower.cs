67da8fceb035aab6e99674242f2f5fe9b364c533
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-29 15:21:43 +0100

    Integration of godzilla and tail

---
+++ Assets/MyAssets/Scripts/VRCameraFollower.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRCameraFollower : MonoBehaviour {
+
+	public Vector3 cameraOffset = Vector3.zero;
+
+	public GameObject VRCameraGO;
+
+	public GameObject neck;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 globalGodzillaPosition = transform.position;
+		Vector3 globalNeckPosition = neck.transform.position;
+
+		Vector3 cameraPosition = VRCameraGO.transform.position;
+
+
+		Vector3 neckDiff = cameraPosition - globalNeckPosition;
+		neckDiff.y = 0.0f;
+		globalGodzillaPosition += neckDiff;
+		transform.position = globalGodzillaPosition;
+
+		float previousYPos = transform.position.y;
+		float previousXRot = transform.eulerAngles.x;
+		float previousZRot = transform.eulerAngles.z;
+
+		//transform.position = new Vector3(VRCameraGO.transform.position.x + cameraOffset.x, 0.0f, VRCameraGO.transform.position.z + cameraOffset.z);
+		transform.eulerAngles  = new Vector3 (previousXRot, VRCameraGO.transform.eulerAngles.y, previousZRot);
+
+
+		globalNeckPosition = neck.transform.position;
+		globalNeckPosition.y = transform.position.y;
+
+		float angle = Vector3.Angle(globalGodzillaPosition - globalNeckPosition, transform.position - globalNeckPosition);
+		transform.RotateAround(globalNeckPosition, Vector3.up, angle);
+
+		transform.position += transform.right * cameraOffset.x;
+		transform.position += transform.forward * cameraOffset.z;
+	}
+}
---
+++ Assets/MyAssets/Scripts/VRGameStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRGameStart : MonoBehaviour {
+	public GameObject character;
+	public GameObject VRCamera;
+
+	// Use this for initialization
+	void Start () {
+		character.SetActive(false);
+		character.transform.position = VRCamera.transform.position;
+		character.transform.rotation = VRCamera.transform.rotation;
+		character.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
