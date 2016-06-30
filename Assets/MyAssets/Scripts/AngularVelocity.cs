8975972c6d51a5219cd024474375c35a1573ef45
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-06-30 18:18:52 +0100

    Tail working, need to work on orientation

---
+++ Assets/MyAssets/Scripts/AngularVelocity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AngularVelocity : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+
+	private Vector3 previousForward;
+	private Vector3 currentForward;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		previousForward = currentForward;
+		currentForward = transform.forward;
+
+		Vector3 distance = currentForward - previousForward;
+		//rigidBody.angularVelocity = distance * (1.0f / Time.fixedTime);
+
+		//Debug.Log(rigidBody.angularVelocity);
+	}
+}
---
+++ Assets/MyAssets/Scripts/MirrorLocation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MirrorLocation : MonoBehaviour {
+
+	public List<Transform> from;
+	public List<Transform> to;
+	public GameObject root;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform t in root.transform) {
+			from.Add (t);
+		}
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!root)
+			return;
+
+		root.transform.position = transform.position;
+		root.transform.rotation = transform.rotation;
+		///if (from == null) {
+		//	return;
+		//}
+
+		for (int i = 0; i < from.Count; i++) {
+			to[i].transform.position = from[i].transform.position;
+			to[i].transform.rotation = from[i].transform.rotation;
+		}
+		//transform.position = from.transform.position;
+		//transform.rotation = from.transform.rotation;
+	}
+}
